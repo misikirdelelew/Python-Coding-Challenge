dc3fca0eb617b7b93965289da4913c48b16cf594
Misikir Delelew <onlinemisikir@gmail.com>
2024-05-30 21:59:18 +0300

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,39 @@
+# name = "Misikir Delelew"  
+# String methods
+
+
+# print(name.count("e") and name.find("Delelew"))
+# print(name.find("Delelew"))
+# print(name.lower())
+
+# print(name.capitalize())
+# print(name.isdigit())
+# print(name.isnumeric())
+# print(name.replace("i","e"))
+
+# Type casting : converting one datatype to another
+# x = 1 
+# y = 3.0
+# z = "3"
+# z = int(z)
+# y = int(y)
+# z = float(z)
+
+# print(x)
+# print(y)
+# print(z*4)
+# print(int(z)*3)
+
+
+# inputs in python
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+
+# age = int(age)
+# height = float(height)
+
+print("your name is: ", name)
+print("you are ", age, " years old")
+print("you are ", height, " meters tall")
