e0be782203a82fab6bc4363039e614973705a59c
Misikir Delelew <onlinemisikir@gmail.com>
2024-06-01 10:50:00 +0300

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,44 @@
+import math
+# mafunction is a builtin function for mathematical operations! 
+
+pi = 3.14
+x = 1
+y = 2
+z =3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.sqrt(pi))
+# print(math.pow(pi,2))
+# print(max(x,y,z))
+# print(min(x,y,z))
+
+#  String Slcing in python
+#  its creating substring from a given string. we can do it with index[],slice(): [start:stop:step]: the first character inclusive, the last character exclusive
+
+name = "Misikir Delelew"
+# first_name = name[0:7]
+# last_name = name[8:]
+# print(first_name)
+# print(last_name)
+
+website = "https://yahoo.com"
+website1 = "https://google.com"
+# 
+# x = slice(8,)
+# y = slice(1,8)
+# print(website[x])
+# print(website1[y])
+
+#  If statement :excutes  the condition is True
+age = int(input("How old are you:"))
+
+# if age == 100:
+    # print("You are century years old!")
+# elif age >= 20:
+    # print("You are an adult!"
+# elif age > 10:
+    # print("you are a child")
+# else:
+    # print("you are so child!")
+    
